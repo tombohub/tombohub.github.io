422e140a9c7a95239c40ec5a9459ec8099f7dc8b
me <tkutlesa@gmail.com>
2024-08-22 10:34:32 -0400

    gh-ages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tombohub.github.io/',
-    outDir: './docs',
     integrations: [sitemap()]
 });
